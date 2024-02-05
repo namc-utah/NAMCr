67ac2c8caad077abb99a64d4d38dec995b65e738
Andrew Caudillo <andrew.caudillo@usu.edu>
2024-02-05 12:51:10 -0700

    = vs <-

--- R/queries.R
+++ R/queries.R
@@ -187,15 +187,15 @@ query = function(
         }
         ##
         ifelse(is.na(page_offset) || nrow( data[[api_endpoint]] ) !=  page_limit || (!is.na(limit) && nrow( data[[api_endpoint]] ) >= limit),
-            is_last_page = TRUE,
-            is_last_page=FALSE)
+            is_last_page <- TRUE,
+            is_last_page<-FALSE)
 
         ifelse(is_paginated && !is.na(paged_data),
-                {data[[api_endpoint]] = paged_data; data[[api_endpoint]] = rbind(paged_data, as.data.frame(data[[api_endpoint]]) )},
+                {data[[api_endpoint]] <- paged_data; data[[api_endpoint]] <- rbind(paged_data, as.data.frame(data[[api_endpoint]]) )},
 
         ifelse(!is.data.frame(paged_data),
-            paged_data = data[[api_endpoint]],
-            paged_data = rbind(paged_data, data[[api_endpoint]])))
+            paged_data <- data[[api_endpoint]],
+            paged_data <- rbind(paged_data, data[[api_endpoint]])))
 
     # Handle JSON in returned data
     if( json_autodetect ){
