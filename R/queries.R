9e2f42fd74c0773e422ee5a7bce6acaf1ea9bec2
Andrew Caudillo <andrew.caudillo@usu.edu>
2024-02-05 13:37:24 -0700

    ..revert

--- R/queries.R
+++ R/queries.R
@@ -186,20 +186,19 @@ query = function(
             )
         }
         ##check for last page conditions
-        is_last_page <- ifelse(
-            is.na(page_offset) || nrow(data[[api_endpoint]]) != page_limit || (!is.na(limit) && nrow(data[[api_endpoint]]) >= limit),
-            {
-                ifelse(
-                    is_paginated && !is.na(paged_data),
-                    {
-                        data[[api_endpoint]] <- paged_data
-                        data[[api_endpoint]] <- rbind(paged_data, as.data.frame(data[[api_endpoint]]))
-                    },
-                    TRUE
-                )
-            },
-            ifelse(!is.data.frame(paged_data), data[[api_endpoint]], rbind(paged_data, data[[api_endpoint]]))
-        )
+        if(is.na(page_offset) || nrow( data[[api_endpoint]] ) !=  page_limit || (!is.na(limit) && nrow( data[[api_endpoint]] ) >= limit) ){
+            is_last_page = TRUE
+            if(is_paginated && !is.na(paged_data)){
+                data[[api_endpoint]] = paged_data
+                data[[api_endpoint]] = rbind(paged_data, as.data.frame(data[[api_endpoint]]) )
+            }
+        } else if(!is.data.frame(paged_data)) {
+            paged_data = data[[api_endpoint]]
+        } else {
+            paged_data = rbind(paged_data, data[[api_endpoint]])
+        }
+
+
 
     # Handle JSON in returned data
     if( json_autodetect ){
